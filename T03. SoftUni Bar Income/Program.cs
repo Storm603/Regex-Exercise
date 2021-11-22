81d73c264f01036ea93b98ddca63b627eb74ab29
Storm603 <93555141+Storm603@users.noreply.github.com>
2021-11-22 23:53:19 +0200

    Add files via upload

---
+++ T03. SoftUni Bar Income/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T03._SoftUni_Bar_Income
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Regex regex = new Regex(@"^[^\|\$%\.]*?%(?<name>[A-Z][a-z]+)%[^\|\$%\.]*?<(?<product>\w+)>[^\|\$%\.]*?\|(?<count>\d+)\|[^\|\$%\.]*?(?<price>\d+(\.\d+)?)\$$");
+
+            string order = Console.ReadLine();
+
+            double totalPrice = 0;
+
+            while (order != "end of shift")
+            {
+                Match validator = regex.Match(order);
+
+                if (validator.Success)
+                {
+                    var name = validator.Groups["name"].Value;
+                    var product = validator.Groups["product"].Value;
+                    double count = double.Parse(validator.Groups["count"].Value);
+                    double price = double.Parse(validator.Groups["price"].Value);
+                    double currentProductPrice = count * price;
+                    Console.WriteLine($"{name}: {product} - {currentProductPrice:f2}");
+                    totalPrice += currentProductPrice;
+                }
+
+                order = Console.ReadLine();
+            }
+            Console.WriteLine($"Total income: {totalPrice:f2}");
+        }
+    }
+}
